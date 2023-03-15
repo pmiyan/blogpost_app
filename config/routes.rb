591f05e1b340543cf0efd89528a4070efe85f5e0
pmiyan <pmiyan@tamu.edu>
2023-03-15 12:51:06 -0400

    added Portfolio via resource generator

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+
   get 'pages/home'
   get 'pages/about'
   get 'pages/contact'
+
   resources :blogs
+
+  resources :portfolios
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
